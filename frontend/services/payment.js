469bb1894750ff7feabc26dc0a38e5fd2cbea519
Sean <seantinashenyakutira@gmail.com>
2026-05-22 10:29:12 +0200

    fix: remove TS type annotations from .js file for Vercel build

--- frontend/services/payment.js
+++ frontend/services/payment.js
@@ -1,13 +1,13 @@
 import api from '../lib/api';
 
 export const paymentService = {
-  enterCompetition: async (competitionId: string) => {
-    return api.post(`/competitions/${competitionId}/enter`).then((res: any) => res);
+  enterCompetition: async (competitionId) => {
+    return api.post(`/competitions/${competitionId}/enter`).then((res) => res);
   },
-  registerVoter: async (competitionId: string) => {
-    return api.post('/voter/register', { competitionId }).then((res: any) => res);
+  registerVoter: async (competitionId) => {
+    return api.post('/voter/register', { competitionId }).then((res) => res);
   },
   getPayments: async () => {
-    return api.get('/payments').then((res: any) => res);
+    return api.get('/payments').then((res) => res);
   },
 };
